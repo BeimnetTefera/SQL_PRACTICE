af19a24c0e411b5b753867ca21976a6873587be2
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-27 16:11:35 +0300

    Create  Rearrange_Products_Table.sql

---
+++ leetcode/Rearrange_Products_Table.sql
@@ -0,0 +1,23 @@
+WITH unpivot_data AS (
+    SELECT 
+        product_id,
+        'store1' AS store,
+        store1 AS price
+    FROM Products
+    UNION 
+    SELECT 
+        product_id,
+        'store2' AS store,
+        store2 AS price
+    FROM Products
+    UNION
+    SELECT 
+        product_id,
+        'store3' AS store,
+        store3 AS price
+    FROM Products
+)
+
+SELECT * 
+FROM unpivot_data
+WHERE price IS NOT NULL;
